b29bd8a9bb4908df7dbc65ea965dc7732da20a16
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-09-19 22:32:39 +0530

    last commit

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.listen(port, () => {
   console.log(`The application started successfully on port ${port}`);
 });
 
 app.get("/", (req, res) => {
-  console.log("This is home page");
-  res.send("<h1>This is home page</h1>");
+  res.render("home.ejs");
 });
-
-app.get("/about", (req, res) => {
-  console.log("This is about page");
-  res.send("<h1>This is about page</h1>");
-});
-
-app.get("/contact", (req, res) => {
-  let { q } = req.query;
-  console.log("This is contact page");
-  res.send("<h1>This is contact page</h1>" + `<h2>Query: ${q}</h2>`);
+app.get("/hello", (req, res) => {
+  res.send("this is hello page");
 });
